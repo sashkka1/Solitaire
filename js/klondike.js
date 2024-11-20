90b1b0a41d022feb4ac2d40f9987b32a7b908780
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-11-20 23:24:24 +0300

    Update klondike.js

--- js/klondike.js
+++ js/klondike.js
@@ -80,6 +80,51 @@ class KlondikeCore extends CardGameCore {
     console.log('2');
     getItemFromCloudStorage("1");
     console.log('3');
+    window.Telegram.WebApp.CloudStorage.getItem("1", (err, array) => {
+      console.table(array);
+      console.table(this._allCards);
+      if (err) {
+        console.error('Error retrieving arrayCardSafe:', err);
+        return; // Exit if there's an error
+      }
+      if (arrayCardSafeOld === null || arrayCardSafeOld === undefined || arrayCardSafeOld === "") {
+        console.log('get empty');
+        console.table(array);
+        window.Telegram.WebApp.CloudStorage.setItem("1", JSON.stringify(this._allCards));
+        return; // Exit if there's an error
+      }else{
+        console.log('get true');
+        console.table(array);
+      }
+
+      window.Telegram.WebApp.CloudStorage.setItem("1", this._allCards);
+    });
+    window.Telegram.WebApp.CloudStorage.getItem("arrayCardSafe", (err, arrayCardSafeOld) => {
+      console.log('arrayCardSafeOld 1');
+      console.table(arrayCardSafeOld);
+      if (err) {
+        console.error('Error retrieving arrayCardSafe:', err);
+        return; // Exit if there's an error
+      }
+      if (arrayCardSafeOld === null || arrayCardSafeOld === undefined || arrayCardSafeOld === "") {
+        console.log('arrayCardSafeOld 2');
+        console.table(arrayCardSafeOld);
+        window.Telegram.WebApp.CloudStorage.setItem("arrayCardSafe", arrayCardSafe);
+        return; // Exit if there's an error
+      }else{
+        this._allCards = JSON.parse(arrayCardSafeOld);
+        // this._allCards = arrayCardSafeOld;
+        console.log('arrayCardSafeOld 3');
+        console.table(this._allCards);
+        this.moveCards(this._allCards, 'stock', false); // Перемещаем все карты в сток
+        for (let i = 0; i < 7; i++) {
+          const howManyCardsToMove = i + 1;
+          const cardsToMove = this.placeIdToCardArray.stock.splice(-howManyCardsToMove); // Извлекаем нужное количество карт из стока
+          this.moveCards(cardsToMove, 'tableau' + i); // Перемещаем их на соответствующее место стола
+          cardsToMove[cardsToMove.length - 1].visible = true; // Открываем последнюю карту в каждом столбце
+        }
+      }
+    });
     // document.getElementById('new-game-button').innerHTML = "Test 9";
     // let arrayCardSafe = Array.from(this._allCards);
     // let tg = window.Telegram.WebApp.CloudStorage;
