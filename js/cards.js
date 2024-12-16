932f3a978b439b8a6dc4988869abf43ae53554f7
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-12-16 18:31:44 +0300

    .

--- js/cards.js
+++ js/cards.js
@@ -19,6 +19,36 @@ let checkcontinue = 0;
 
 
 
+block = document.getElementById('check-desire-button-ok');
+  block.addEventListener('click', () => {
+    console.log('клик на ok в панели выбора');
+    gameIsStart=0;
+    window.Telegram.WebApp.CloudStorage.removeItem("saveCard");
+    // localStorage.removeItem("saveCard");
+    let elements = document.getElementById("check-desire-box");
+    elements.classList.remove('normal');
+    this.currentGame = new this._CoreClass(Array.from(this.cardDivs.keys()), ...arguments);
+    this.currentGame.addEventListener('CardsMoved', event => this._onCardsMoved(event)); // Подписка на событие перемещения карт
+    this.currentGame.deal(); // Начало игры (раздача карт)
+    super.newGame(); // Вызов метода newGame() родительского класса
+    this.currentGame.stockCurrentDefolt();
+    autocomplete.classList.remove('normal-auto');
+    buttonPlace.classList.add('normal');
+
+    if(checkcontinue > 0){ 
+      let occurrence_time_local = new Date(); //Старт новой игры, если прошлая игра находилась в прогрессеПо сути дубликат level_start (отправится два ивента), но будет указание на то, что пользователь решил пропустить текущую игру
+      let occurrence_time_utc0 = new Date().toISOString();
+      gtag('event', 'level_reset_and_start', {
+        'occurrence_time_local': occurrence_time_local,
+        'occurrence_time_utc0': occurrence_time_utc0,
+        'game_version': game_version,
+      });
+      checkcontinue=0;
+    }
+  });
+
+
+
 
 // кнопка отмены при проверке создания новой игры
 block = document.getElementById('check-desire-button-cancel');
@@ -479,37 +509,6 @@ export class CardGameUI extends GameUI {
     this._draggingState = null; // Состояние для отслеживания процесса перетаскивания
   }
 
-
-asdf(){
-    let block = document.getElementById('check-desire-button-ok');
-    block.addEventListener('click', () => {
-    console.log('клик на ok в панели выбора');
-    gameIsStart=0;
-    window.Telegram.WebApp.CloudStorage.removeItem("saveCard");
-    // localStorage.removeItem("saveCard");
-    let elements = document.getElementById("check-desire-box");
-    elements.classList.remove('normal');
-    this.currentGame = new this._CoreClass(Array.from(this.cardDivs.keys()), ...arguments);
-    this.currentGame.addEventListener('CardsMoved', event => this._onCardsMoved(event)); // Подписка на событие перемещения карт
-    this.currentGame.deal(); // Начало игры (раздача карт)
-    super.newGame(); // Вызов метода newGame() родительского класса
-    this.currentGame.stockCurrentDefolt();
-    autocomplete.classList.remove('normal-auto');
-    buttonPlace.classList.add('normal');
-
-    if(checkcontinue > 0){ 
-      let occurrence_time_local = new Date(); //Старт новой игры, если прошлая игра находилась в прогрессеПо сути дубликат level_start (отправится два ивента), но будет указание на то, что пользователь решил пропустить текущую игру
-      let occurrence_time_utc0 = new Date().toISOString();
-      gtag('event', 'level_reset_and_start', {
-        'occurrence_time_local': occurrence_time_local,
-        'occurrence_time_utc0': occurrence_time_utc0,
-        'game_version': game_version,
-      });
-      checkcontinue=0;
-    }
-    
-  });
-}
 
   newGame() {// проверяю на то была ли игра уже начата чтобы решить выставлять блок решением пользователя о продолжении или новой игре
     
