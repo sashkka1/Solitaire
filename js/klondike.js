2380e058832edd11bd57f15014bb7c20dde87e6a
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2024-12-20 22:24:46 +0300

    ю

--- js/klondike.js
+++ js/klondike.js
@@ -1,5 +1,6 @@
   // Import the functions you need from the SDKs you need
-  import { getAnalytics, logEvent, initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics, logEvent, } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
